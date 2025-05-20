442732b22f2b6407171c574494d68d314df9d50d
Christina Dobbins <u800176908@byui.edu>
2025-05-20 13:56:35 -0600

    Add cars data module and update index route to include cars

---
+++ data/cars.js
@@ -0,0 +1,5 @@
+const cars = [
+    { name: "Toyota Camry", description: "Reliable and efficient sedan." },
+    { name: "Ford Mustang", description: "Classic American muscle car." }
+];
+module.exports = cars;
