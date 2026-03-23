0a2bd701083d0c949cdb26176f7e97c2119d0121
Christina Dobbins <u800176908@byui.edu>
2026-03-23 12:45:44 -0600

    Format error message object in file not found route

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use("/upgrades", upgradesRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-    next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+    next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 })
 
 /* ***********************
