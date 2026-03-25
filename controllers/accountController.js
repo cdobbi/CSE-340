a53eeaae84b3be77adfcb3d2ed0567e6eb8a47a0
Christina Dobbins <u800176908@byui.edu>
2026-03-24 20:04:09 -0600

    Add login view and update buildLogin function to include navigation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,6 +4,7 @@ const utilities = require("../utilities")
 * Deliver login view
 * *********************** */
 async function buildLogin(req, res, next) {
+    let nav = await utilities.getNav()
     res.render("account/login", {
         title: "Login",
         nav,
