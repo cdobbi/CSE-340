9c306f0136ca2e4e46598c6f438c1dcc640342dd
Christina Dobbins <u800176908@byui.edu>
2026-03-24 19:13:56 -0600

    Fix typo in login route handler function name

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,6 @@ const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 
-router.get("/login", utilities.handleErrors(accountController.buildlogin))
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
 module.exports = router;
