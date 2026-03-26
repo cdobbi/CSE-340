a18448b41fadbb0f02acef8c368a959f904b8271
Christina Dobbins <u800176908@byui.edu>
2026-03-26 08:03:48 -0600

    Add registration data validation middleware to registration route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,11 +1,14 @@
+const regValidate = require('../utilities/account-validation')
+
 const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 
+
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Process the registration data
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
