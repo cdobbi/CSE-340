bd1b585de12950dc582e984ab2822c80fd393631
Christina Dobbins <u800176908@byui.edu>
2026-04-06 13:56:27 -0600

    Add validation rules for account updates and password strength checks

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -168,4 +168,59 @@ async function updatePassword(account_id, account_password) {
     }
 }
 
-module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildAccountManagement, accountLogout, getAccountById, updateAccount, updatePassword }
+validate.updateAccountRules = () => {
+    return [
+        body("account_firstname")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 1 })
+            .withMessage("Please provide a first name."),
+
+        body("account_lastname")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 2 })
+            .withMessage("Please provide a last name."),
+
+        body("account_email")
+            .trim()
+            .isEmail()
+            .normalizeEmail()
+            .withMessage("A valid email is required."),
+    ]
+}
+
+validate.updatePasswordRules = () => {
+    return [
+        body("account_password")
+            .trim()
+            .notEmpty()
+            .isStrongPassword({
+                minLength: 12,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1,
+            })
+            .withMessage("Password does not meet requirements."),
+    ]
+}
+
+validate.checkUpdateData = async (req, res, next) => {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        let nav = await utilities.getNav()
+        res.render("account/index", {
+            errors,
+            title: "Account Management",
+            nav,
+            accountData: res.locals.accountData,
+        })
+        return
+    }
+    next()
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildAccountManagement, accountLogout, getAccountById, updateAccount, updatePassword, validate }
